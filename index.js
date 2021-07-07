941467bdb5de7b8ddef8d4dec0452d9b55ef113a
Philipp Kant <ptckant@gmail.com>
2021-07-07 11:09:56 +0200

    changed rule and fixed error

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const twitterClient = new Twit({
 
 const current_year = new Date().getFullYear()
 const hundreds = Array.from({length:50},(v,k)=>k*100+100)
-const repdigits = Array.from({length:9},(v,k)=>k*111+222)
+const repdigits = Array.from({length:9},(v,k)=>k*111+111)
 const repdigitsBig = Array.from({length:9},(v,k)=>k*1111+1111)
 const tens = Array.from({length:9},(v,k)=>k*10+10)
 const ones = Array.from({length:9},(v,k)=>k+1)
-const otherYears = [150, 250]
+const otherYears = [110, 120, 130, 140, 150, 250, 350, 450, 550, 650, 750, 850, 950]
 const years = hundreds.concat(repdigits, repdigitsBig, ones, tens, otherYears)
 
 function yearToInt(year) {
@@ -33,15 +33,17 @@ axios
   .then(response => {
     const data = response.data.data ? response.data.data : {}
     let tweet
-    var largestDiff = 0
+    var largestDiff = -1
     var largestDiffIdx = 0
     if (data.Events && data.Events.length) {
       data.Events.forEach((element, index) => {
         diff_year = current_year - yearToInt(element.year)
         if (years.includes(diff_year)) {
           if (diff_year > largestDiff) {
-            largestDiff = diff_year
-            largestDiffIdx = index
+            if (!((largestDiff % 100 == 0 || largestDiff % 111 == 0) && otherYears.includes(diff_year))) {
+              largestDiff = diff_year
+              largestDiffIdx = index
+            }
           }
         }
       })
@@ -54,8 +56,8 @@ axios
       if (text.length > 237) {
         text = text.substring(0,237) + "..."
       }
-      if (largestDiff == 0) {
-        largestDiff = current_year - yearToInt(element.year)
+      if (largestDiff == -1) {
+        largestDiff = current_year - yearToInt(data.Events[0].year)
       }
 
       tweet = "Today " + largestDiff + " years ago:\n"
